3daf493d9cd4a3cb2227268854b9c5690efb76c3
shrpne <shrpne@gmail.com>
2018-08-11 18:54:03 +0300

    rename unbound to unbond

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,10 +1,10 @@
 import ethUtil from 'ethereumjs-util';
 import MinterDeclareCandidacyTxData from 'minterjs-tx/src/tx-data/declare-candidacy';
 import MinterDelegateTxData from 'minterjs-tx/src/tx-data/delegate';
-import MinterUnboundTxData from 'minterjs-tx/src/tx-data/unbound';
+import MinterUnbondTxData from 'minterjs-tx/src/tx-data/unbond';
 import MinterSetCandidateOnTxData from 'minterjs-tx/src/tx-data/set-candidate-on';
 import MinterSetCandidateOffTxData from 'minterjs-tx/src/tx-data/set-candidate-off';
-import {TX_TYPE_DECLARE_CANDIDACY, TX_TYPE_DELEGATE, TX_TYPE_UNBOUND, TX_TYPE_SET_CANDIDATE_ON, TX_TYPE_SET_CANDIDATE_OFF} from 'minterjs-tx/src/tx-types';
+import {TX_TYPE_DECLARE_CANDIDACY, TX_TYPE_DELEGATE, TX_TYPE_UNBOND, TX_TYPE_SET_CANDIDATE_ON, TX_TYPE_SET_CANDIDATE_OFF} from 'minterjs-tx/src/tx-types';
 import converter from 'minterjs-tx/src/converter';
 import {formatCoin} from 'minterjs-tx/src/helpers';
 import {toBuffer} from 'minterjs-util';
@@ -81,7 +81,7 @@ export function delegateTx({privateKey, publicKey, coinSymbol, stake, feeCoinSym
  * @return {TxParams}
  */
 export function unbondTx({privateKey, publicKey, coinSymbol, stake, feeCoinSymbol, message}) {
-    const txData = new MinterUnboundTxData({
+    const txData = new MinterUnbondTxData({
         pubkey: toBuffer(publicKey),
         coin: formatCoin(coinSymbol),
         stake: `0x${converter.convert(stake, 'pip').toString(16)}`,
@@ -95,7 +95,7 @@ export function unbondTx({privateKey, publicKey, coinSymbol, stake, feeCoinSymbo
         privateKey,
         message,
         gasCoin: feeCoinSymbol,
-        txType: TX_TYPE_UNBOUND,
+        txType: TX_TYPE_UNBOND,
         txData: txData.serialize(),
     };
 }
