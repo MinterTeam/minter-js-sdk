27fbb62a3bcca6ceaf9cd44018f3d655c8a11d6f
shrpne <shrpne@gmail.com>
2019-05-14 01:41:11 +0300

    fix post-signed-tx file

---
+++ src/api/post-signed-tx.js
@@ -0,0 +1,43 @@
+import {Buffer} from 'safe-buffer';
+import {API_TYPE_GATE} from '../variables';
+import {getData} from './utils';
+
+/**
+ * @param {MinterApiInstance} apiInstance
+ * @return {Function<Promise>}
+ * @constructor
+ */
+export default function PostSignedTx(apiInstance) {
+    /**
+     * @param {string|Buffer} signedTx
+     * @return {Promise<string>}
+     */
+    return function postSignedTx(signedTx) {
+        if (Buffer.isBuffer(signedTx)) {
+            signedTx = signedTx.toString('hex');
+        }
+
+        let postTxPromise;
+        if (apiInstance.defaults.apiType === API_TYPE_GATE) {
+            postTxPromise = apiInstance.post('/api/v1/transaction/push', {
+                transaction: signedTx,
+            });
+        } else {
+            postTxPromise = apiInstance.get(`/send_transaction?tx=0x${signedTx}`);
+        }
+
+        return postTxPromise
+            .then((response) => {
+                const resData = getData(response, apiInstance.defaults.apiType);
+                let txHash = resData.hash.toLowerCase();
+                // @TODO is transform needed?
+                if (txHash.indexOf('mt') !== 0) {
+                    txHash = `Mt${txHash}`;
+                } else {
+                    txHash = txHash.replace(/^mt/, 'Mt');
+                }
+
+                return txHash;
+            });
+    };
+}
